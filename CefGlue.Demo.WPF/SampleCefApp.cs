e36ce8614ff1900c6f78e91d2e6bd174cb067b91
Vibeeshan <m.vibeeshan@mubasher.net>
2017-11-26 11:29:21 +0530

    Initial Commit

---
+++ CefGlue.Demo.WPF/SampleCefApp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xilium.CefGlue;
+
+namespace CefGlue.Demo.WPF
+{
+    internal sealed class SampleCefApp : CefApp
+    {
+        public SampleCefApp()
+        {
+        }
+    }
+}
